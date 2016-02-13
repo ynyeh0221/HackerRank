bdac880b5009599c2a0af4848e9399826ea9aaa2
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-02-12 22:07:04 -0500

    Update String Similarity.js

--- String Similarity.js
+++ String Similarity.js
@@ -3,26 +3,25 @@
 function processData(input) {
     //Enter your code here
     input=input.split("\n");
-    for(i=1;i<=parseInt(input[0]);i++)
+    for(k=1;k<=parseInt(input[0]);k++)
     {
-        var s=input[i].split("");
-        var s2=input[i].split("");
-        s2[0]=0;
-        var ind=s2.indexOf(s[0]);
-        var sum=0;
-        while(ind!=-1)
+        var str=input[k];
+        var len=input[k].length;
+        var count=0;
+        var total=0;
+        for(i=1;i<len;i++)
         {
-            for(j=ind;j<s.length;j++)
+            count=0;
+            for(j=i;j<len;j++)
             {
-                if(s[j]==s[j-ind])
-                    sum++;
+                if(str.charAt(j-i)==str.charAt(j))
+                    count++;
                 else
                     break;
             }
-            s2[ind]=0;
-            ind=s2.indexOf(s[0]);
+            total=total+count;
         }
-        console.log(sum+s.length);
+        console.log(total+len);
     }
 } 
 
