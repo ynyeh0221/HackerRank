4b10e06649d7e739751dbee4fb94a3074fc8d83b
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-02-14 12:04:56 -0500

    Create Print the Elements of a Linked List.py

---
+++ Print the Elements of a Linked List.py
@@ -0,0 +1,17 @@
+"""
+ Print elements of a linked list on console
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+ 
+ 
+"""
+def print_list(head):
+    while(head!=None):
+        print head.data
+        head=head.next
