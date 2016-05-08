a0f1c30ab9ab414eda64df91f42e54d879317a81
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-05-08 11:42:51 -0400

    Create Preprocessor Solution.cpp

---
+++ Preprocessor Solution.cpp
@@ -0,0 +1,10 @@
+/* Enter your macros here */
+
+#include<iostream>
+using namespace std;
+
+#define toStr(s) #s
+#define foreach(v,i) for(int i=0;i<n;i++)
+#define io(v) int l=0; cin>>l; vector <int> vv(v.size()); vv[i]=l; v[i]=vv 
+#define FUNCTION(a,b) void a(int& x,int& y){if( y b x ) x=y;}
+#define INF 100000000
