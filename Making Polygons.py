e7b8f2616bfbe049f007e2f8cfef6169f1c9070d
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-08-15 09:55:21 -0400

    Update Making Polygons.py

--- Making Polygons.py
+++ Making Polygons.py
@@ -6,8 +6,13 @@ a = map(int,raw_input().strip().split(' '))
 
 if n == 1:
     print 2
-res = 0
-for i in a:
-    if i >= sum(a)/2:
-        res += 1
-print res
+elif n == 2:
+    if a[0] == a[1]:
+        print 2
+    else:
+        print 1
+elif n >= 3:
+    if sum(a) <= 2 * max(a):
+        print 1
+    else:
+        print 0
