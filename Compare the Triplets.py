4ab0732e73d5092a9ca62dd98df6c4c590bc88e3
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-09-01 08:37:19 -0400

    Update Compare the Triplets.py

--- Compare the Triplets.py
+++ Compare the Triplets.py
@@ -4,22 +4,14 @@ import sys
 
 
 a0,a1,a2 = raw_input().strip().split(' ')
-a0,a1,a2 = [int(a0),int(a1),int(a2)]
+a = [int(a0),int(a1),int(a2)]
 b0,b1,b2 = raw_input().strip().split(' ')
-b0,b1,b2 = [int(b0),int(b1),int(b2)]
+b = [int(b0),int(b1),int(b2)]
 
-score=[0,0]
-
-if a0>b0:
-    score[0]+=1
-elif a0<b0:
-    score[1]+=1
-if a1>b1:
-    score[0]+=1
-elif a1<b1:
-    score[1]+=1
-if a2>b2:
-    score[0]+=1
-elif a2<b2:
-    score[1]+=1
-print " ".join(str(e) for e in score)
+A =B =0
+for i in xrange(3):
+    if a[i] > b[i]:
+        A += 1
+    elif a[i] < b[i]:
+        B += 1
+print str(A) + ' ' + str(B)
