7dbc0a8ec9d54db5f989b46af5caa35e53c2b2b3
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-09-14 11:45:32 -0400

    Update Kruskal (MST): Really Special Subtree.py

--- Kruskal (MST): Really Special Subtree.py
+++ Kruskal (MST): Really Special Subtree.py
@@ -2,9 +2,8 @@
 
 import sys
 from operator import itemgetter
-from sets import Set
 
-def Kruskal(V,adj,mat):
+def Kruskal(V,mat):
     mstsum=0
     mat=sorted(mat,key=itemgetter(2))
     while (len(mat)>0):
@@ -20,21 +19,12 @@ def Kruskal(V,adj,mat):
 s=raw_input().split()
 N=int(s[0])
 M=int(s[1])
-adj=[[-1 for i in range(N)] for j in range(N)]
-for i in range(N):
-    adj[i][i]=0
 mat=[]
 for m in xrange(M):
-    s=raw_input().split()
-    x=int(s[0])
-    y=int(s[1])
-    r=int(s[2])
-    adj[x-1][y-1]=r
-    adj[y-1][x-1]=r
-    s=map(int,s)
+    s=map(int,raw_input().split())
     mat.append(s)
-start=int(raw_input())
+start=1
 V=[]
 for i in xrange(1,N+1):
     V.append(set([i]))
-Kruskal(V,adj,mat)
+Kruskal(V,mat)
