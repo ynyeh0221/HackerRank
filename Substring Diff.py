c69dde57826dd308091b04a5833936913a3e5bf8
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-09-30 20:45:26 -0400

    Update Substring Diff.py

--- Substring Diff.py
+++ Substring Diff.py
@@ -3,7 +3,7 @@ for d in xrange(D):
     [S, P, Q] = raw_input().split()
     S = int(S)
     n = len(P)
-    diff = [[0 for j in xrange(n)] for i in xrange(n)]
+    diff = [[0 for j in xrange(1500)] for i in xrange(1500)]
     for i in xrange(n):
         for j in xrange(n):
             if P[i] != Q[j]:
@@ -15,10 +15,10 @@ for d in xrange(D):
         for end in xrange(0, n-i):
             sum_before_end1 += diff[end][i+end]
             sum_before_end2 += diff[i+end][end]
-            while i+start1 < n and sum_before_end1 - sum_before_start1 > S:
+            while i+start1 < 1499 and sum_before_end1 - sum_before_start1 > S:
                 start1 += 1
                 sum_before_start1 += diff[start1][i+start1]
-            while i+start2 < n and sum_before_end2 - sum_before_start2 > S:
+            while i+start2 < 1499 and sum_before_end2 - sum_before_start2 > S:
                 start2 += 1
                 sum_before_start2 += diff[i+start2][start2]
 
