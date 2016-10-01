92618d27ef585383e1167ca187d4edb7c528eff5
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-10-01 18:25:54 -0400

    Create Equal.py

---
+++ Equal.py
@@ -0,0 +1,16 @@
+import sys
+N = int(raw_input())
+for nn in xrange(N):
+    n = int(raw_input())
+    A = map(int, raw_input().split())
+    minn = sys.maxint
+    for i in A:
+        minn = min(minn, i)
+    res = sys.maxint
+    for i in xrange(-5, 1):
+        temp = 0
+        for j in xrange(n):
+            delta = A[j] - i - minn
+            temp += delta/5 + delta%5/2 + delta%5%2
+        res = min(temp, res)
+    print res
