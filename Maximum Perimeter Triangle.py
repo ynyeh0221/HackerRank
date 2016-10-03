0bf08bcfac3d8b6a05cf39364fb4eeb6a745b212
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-10-03 16:12:23 -0400

    Update Maximum Perimeter Triangle.py

--- Maximum Perimeter Triangle.py
+++ Maximum Perimeter Triangle.py
@@ -1,21 +1,17 @@
-from operator import itemgetter
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n=int(raw_input())
-s=raw_input().split()
-num=map(int,s)
-num=sorted(num)
-
-result=[]
-for i in xrange(n-1,-1,-1):
+N = int(raw_input())
+A = map(int, raw_input().split())
+A = sorted(A)
+found = False
+for i in xrange(N-1,-1,-1):
     for j in xrange(i-1,-1,-1):
         for k in xrange(j-1,-1,-1):
-            if i!=j and j!=k and i!=k:
-                if num[i]+num[j]>num[k] and num[i]+num[k]>num[j] and num[j]+num[k]>num[i]:
-                    result.append([num[k],num[j],num[i]])
-                    
-result=sorted(result, key=itemgetter(2), reverse=True)
-result=sorted(result, key=itemgetter(0), reverse=True)
-if len(result)>0:
-    print ' '.join(str(e) for e in result[0])
-else:
+            if A[j]+A[k] > A[i]:
+                print ' '.join(str(e) for e in [A[k],A[j],A[i]])
+                found = True
+                break
+        if found:
+            break
+    if found:
+        break
+if not found:
     print -1
