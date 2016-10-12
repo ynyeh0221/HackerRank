c434f8e63d31cc9dff4b009288f63062037722a2
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-10-11 21:27:05 -0400

    Create Python If-Else.py

---
+++ Python If-Else.py
@@ -0,0 +1,16 @@
+#!/bin/python
+
+import sys
+
+
+N = int(raw_input().strip())
+
+if N % 2 == 0:
+    if N >= 2 and N <= 5:
+        print "Not Weird"
+    elif N >= 6 and N <= 20:
+        print "Weird"
+    elif N > 20:
+        print "Not Weird"
+else:
+    print "Weird"
