87022e346720085410402da2f9a73d6d4221e81b
Yi-Nung Yeh <ynyeh0221@gmail.com>
2016-10-17 20:19:44 -0400

    Create Java Hashset.java

---
+++ Java Hashset.java
@@ -0,0 +1,6 @@
+        HashMap h = new HashMap();
+        for (int i = 0; i < t; i++)
+        {
+            h.put(pair_left[i] + " " + pair_right[i], 1);
+            System.out.println(h.size());
+        }
