dd37cd6d3cc525d3c26fd17bffbe3389be01642a
Ananth <67368856+iamananth@users.noreply.github.com>
2022-08-17 23:31:25 +0530

    Some Change will make it work.
    The previous version doesn't work because it produces a run time error.

--- Java End-of-file.java
+++ Java End-of-file.java
@@ -9,12 +9,9 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        int i = 1;
-        while (str != null)
-        {
-            System.out.print(Integer.toString(i++) + ' ' + str + '\n');
-            str = scanner.nextLine();
+        int i = 0;
+        while (sc.hasNextLine()) {
+            System.out.println(++i + " " + sc.nextLine());
         }
     }
 }
